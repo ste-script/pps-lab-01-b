bba9fb079203d345fef8ffafeed996b1db2432b6
Stefano Babini <stefano.babini5@studio.unibo.it>
2024-03-05 09:27:15 +0100

    green flag

--- src/main/java/e1/Knight.java
+++ src/main/java/e1/Knight.java
@@ -2,28 +2,35 @@ package e1;
 
 public class Knight implements ChessPiece {
 
+    private Pair<Integer, Integer> position;
+
+    public Knight() {
+        this.position = new Pair<>(0, 0);
+    }
+
     @Override
     public Pair<Integer, Integer> getPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+        return this.position;
     }
 
     @Override
     public boolean isValidMove(int row, int col) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isValidMove'");
+        int x = row - this.position.getX();
+        int y = col - this.position.getY();
+        if (x != 0 && y != 0 && Math.abs(x) + Math.abs(y) == 3) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void setPosition(int row, int col) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+        this.position = new Pair<>(row, col);
     }
 
     @Override
     public boolean isAtPosition(int row, int col) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAtPosition'");
+        return this.position.equals(new Pair<>(row, col));
     }
 
 }
--- src/test/java/e1/KnightTest.java
+++ src/test/java/e1/KnightTest.java
@@ -1,6 +1,8 @@
 package e1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,36 @@ public class KnightTest {
     @Test
     public void testInitialPosition() {
         assertEquals(new Pair<Integer, Integer>(0, 0), knight.getPosition());
+    }
+
+    @Test
+    public void testValidMove() {
+        knight.setPosition(0, 0);
+        assertTrue(knight.isValidMove(2, 1));
+    }
+
+    @Test
+    public void testInvalidMove() {
+        knight.setPosition(0, 0);
+        assertFalse(knight.isValidMove(2, 2));
+    }
+
+    @Test
+    public void testSetPosition() {
+        knight.setPosition(2, 1);
+        assertEquals(new Pair<Integer, Integer>(2, 1), knight.getPosition());
+    }
+
+    @Test
+    public void testIsAtPosition() {
+        knight.setPosition(2, 1);
+        assertTrue(knight.isAtPosition(2, 1));
+    }
+
+    @Test
+    public void testIsNotAtPosition() {
+        knight.setPosition(2, 1);
+        assertFalse(knight.isAtPosition(0, 0));
     }
 
 }
